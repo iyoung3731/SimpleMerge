9a66876c7b583a0dc6b2a973d9ca91c98bd11d7f
iyoung3731 <43254466+iyoung3731@users.noreply.github.com>
2019-02-16 16:30:23 -0500

    Update SimpleMerge.java

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -1,3 +1,4 @@
+package com.company;
 /**
  * Created by Teacher on 1/28/2019.
  * simpleMerge takes in two sorted arrays of
@@ -5,8 +6,37 @@
  * array of integers.
  */
 public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2) {
-
+    public static int[] simpleMerge(int[] arr1, int[] arr2)
+    {
+        int arr3 [] = new int[arr1.length+arr2.length];
+        int count = 0;
+        int x = 0; 
+        int y = 0;
+        while ((x<arr1.length-1) && (y<arr2.length-1))
+        {
+            if (arr1[x] < arr2[y])
+            {
+                arr3[count] = arr1[x];
+                count++;
+                x++;
+            }
+            else
+            {
+                arr3[count] = arr2[y];
+                count++;
+                y++;
+            }
+        }
+        while(x<arr1.length){
+            arr3[count] = arr1[x];
+            count++;
+            x++;
+        }
+        while(y<arr2.length){
+            arr3[ind] = arr2[y];
+            count++;
+            y++;        
+        }
+        return arr3;
     }
-
 }
